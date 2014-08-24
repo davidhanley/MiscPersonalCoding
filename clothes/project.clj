236b4b7e7d9968af024945f0f62b5d49e962aa1b
david hanley <ctchrinthry@gmail.com>
2014-08-23 22:58:58 -0500

    no need for combinatorics

--- clothes/project.clj
+++ clothes/project.clj
@@ -3,8 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/math.combinatorics "0.0.8"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :main ^:skip-aot clothes.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
